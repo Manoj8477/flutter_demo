27625a85429e1ebf8e754cc46e18e12ac08885a9
Manoj8477 <82501325+Manoj8477@users.noreply.github.com>
2022-11-27 11:49:44 +0530

    First Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: avoid_unnecessary_containers, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final days = 30;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of Flutter"),
+        ),
+      ),
+      drawer:const Drawer(),
+    );
+  }
+}
